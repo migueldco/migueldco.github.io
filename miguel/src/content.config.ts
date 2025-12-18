df94b778f887f2de785436fb1150689cba8de2a8
Miguel <migueldco14@gmail.com>
2025-12-18 16:41:39 -0300

    move projects to a collection

--- miguel/src/content.config.ts
+++ miguel/src/content.config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-	loader: glob({ pattern: '*.md', base: './src/content/blog'}),
+	loader: glob({ pattern: '*.md', base: './src/content/blog' }),
 	schema: z.object({
 		title: z.string(),
 		author: z.string(),
@@ -13,5 +13,16 @@ const blog = defineCollection({
 	}),
 });
 
+const projects = defineCollection({
+	loader: glob({ pattern: '*.yaml', base: './src/content/projects' }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		tags: z.array(z.string()),
+		link: z.string().optional(),
+		image: z.string().optional(),
+		featured: z.boolean().optional(),
+	}),
+});
 
-export const collections = { blog };
+export const collections = { blog, projects };
